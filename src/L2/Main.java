1171cab8dee70aa3b29174bccee5e1e530609e2b
nmayne <nmayne@ualberta.ca>
2017-10-11 14:40:16 -0600

    Newest version of PID

--- src/L2/Main.java
+++ src/L2/Main.java
@@ -5,6 +5,8 @@ import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
 
+import java.util.Random;
+
 import L2.PIDController;
 
 public class Main {
@@ -14,17 +16,36 @@ public class Main {
 		
 		UnregulatedMotor M1 = new UnregulatedMotor(MotorPort.A);
 		PIDController PIDController1 = new PIDController(M1);
-
-		int angles[] = {-360, -180, -90, 90, 180, 360};
-			
-		for(int i = 0; i < angles.length; i++) {
-			result = PIDController1.PID(angles[i], 5, 1, 0, 300, 500);
-			while(result==null) {}
-			System.out.println(result);
-			Delay.msDelay(100);
-			result = null;
-		}
+		Random rand = new Random();
+		
+//		result = PIDController1.PID(90, 2, 1, 0, 100);
+//		System.out.println(result);
+//		Delay.msDelay(1000);
 //		Button.waitForAnyPress();	// press any button to exit
-	}
+		
+//		int angles[] = {90};
+		int angles[] = {-180, -90, 90, 180};			
+		
+//		double Kp = 7;
+		double Ki = 0;
+		double Kd = 0;
+		
+		for(double Kp = 1; Kp < 15; Kp++) {
+//		for(double Kd = 100; Kd < 1000; Kd = Kd + 10) {
 
+				for(int i = 0; i < angles.length; i++) {
+					System.out.printf("P:%.1f I:%.1f D:%.1f\n", Kp, Ki, Kd);
+
+//					result = PIDController1.PID(rand.nextInt()%360, Kp, Ki, Kd, 100);
+					result = PIDController1.PID(angles[i], Kp, Ki, Kd, 100);
+
+					System.out.println(result);
+					Delay.msDelay(1000);
+//					Button.waitForAnyPress();	// press any button to exit
+				}
+			}
+
+		
+		
+	}
 }
--- src/L2/PIDController.java
+++ src/L2/PIDController.java
@@ -16,12 +16,12 @@ public class PIDController {
 	double prevError = 0;	// error from previous sample
 	double derivative = 0;	// the derivative error
 	double integral = 0;		// the integral error
-	double time = 0;			// delta t
-	double power = 0;		// power to motor
+	double delta_t = 0;		// delta t
+	int power = 0;			// power to motor
 
 	
 	/*
-	 * need a motor
+	 * 
 	 */
 	public PIDController(UnregulatedMotor motor) {
 		M = motor;
@@ -33,39 +33,50 @@ public class PIDController {
 	 * PID controller
 	 * target angle of rotation (deg)
 	 */
-	public String PID(int target, double Kp, double Ki, double Kd, int speedMax, int inputRange) {
+	public String PID(int target, double Kp, double Ki, double Kd, int powerMax) {
 				
-		error = target;	// error
-		prevError = 1;	// error from previous sample
-		derivative = 0;	// the derivative error
-		integral = 0;	// the integral error
-		time = 0;		// delta t
-		power = 0;		// power to motor
+		error = target;		// error
+		prevError = error;	// error from previous sample
+		derivative = 0;		// the derivative error
+		integral = 0;		// the integral error
+		delta_t = 1;			// delta t
+		power = 0;			// power to motor
 		
 		M.resetTachoCount();
 		timer.reset();
 		
-		while (Math.abs(prevError) > 0){
-			time = timer.elapsed();
+		while (error != 0 || prevError != 0){
+			integral = integral + (error * delta_t);
+			derivative = (error - prevError) / delta_t;
+			power = (int) (Math.min(powerMax, Math.abs((Kp * error) + (Ki * integral) + (Kd * derivative))));
+			M.setPower(isPos(error) * power);
+			Delay.msDelay(10);					// this delay! grrr, works reasonably well when < 15
+												// the dervative only seems to have a meaningful effect
+												// when it is removed altogether, but removing it altogether
+												// causes bad behaviour in otherways, and it is also unclear
+												// why the derivative is not functioning meaningfully when
+												// there is a delay, even of as little as 1 ms!
+			
+			// get error and delta t
+			prevError = error;
+			error = target-M.getTachoCount();
+			delta_t = timer.elapsed();
 			timer.reset();
-						
-			error = target-M.getTachoCount();		// get proportional error
-			integral = integral + (error*time);		// compute integral
-			derivative = (error - prevError) / time; // compute derivative
-			prevError = error;						// save the error
-
-			// compute and apply power
-			power = Math.abs((Kp * error) + (Ki * integral) + (Kd * derivative));	
-			M.setPower(map((isPos(error) *Math.min(power, inputRange)),-inputRange, inputRange, -speedMax, speedMax));
-			System.out.println(error);
-		}			
-
+		}
+		
 		M.setPower(0); // halt motor
+		Delay.msDelay(500);					// wait for motor to fully halt
+		error = target-M.getTachoCount();	// get error again because the motor has possibly moved
+											// a little after we exited the while loop
 		return ("Error: " + error + "\nTacho: " + M.getTachoCount() + "\n");
 	}
 	
 	/*
-	 * is e pos or neg?
+	 * is e pos or neg? Used to get the direction of the error... but come to think of it the Math.abs above might be 
+	 * the reason things aren't working for me. Try removing that, I can't recall why I did that in the first place!
+	 * Oooooh, because the clipping using Math.min currently needs an unsigned magnitude to work properly... maybe
+	 * there is a better way of dividing that up... but really, the direction of the error should be the direction
+	 * of the power so that we head closer toward the target every time unless we overshoot.
 	 */
 	int isPos(double e) {
 		if (e < 0){
@@ -73,13 +84,5 @@ public class PIDController {
 		} else {
 			return 1;
 		}	
-	}
-	
-	/*
-	 * https://stackoverflow.com/questions/7505991/arduino-map-equivalent-function-in-java
-	 */
-	int map(double x, double in_min, double in_max, double out_min, double out_max)
-	{
-	  return (int) ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
 	}
 }
