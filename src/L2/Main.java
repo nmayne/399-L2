d04f645a89104821cceda884fa4f3dd9edb6c822
nmayne <nmayne@ualberta.ca>
2017-10-13 15:56:05 -0600

    PID Complete

--- src/L2/Main.java
+++ src/L2/Main.java
@@ -18,30 +18,30 @@ public class Main {
 		PIDController PIDController1 = new PIDController(M1);
 		Random rand = new Random();
 		
-//		result = PIDController1.PID(90, 2, 1, 0, 100);
+//		result = PIDController1.PID(60, 10, 0, 0, 100);
 //		System.out.println(result);
 //		Delay.msDelay(1000);
 //		Button.waitForAnyPress();	// press any button to exit
 		
-//		int angles[] = {90};
-		int angles[] = {-180, -90, 90, 180};			
+//		int angles[] = {1080};
+		int angles[] = {-180, 90};			
 		
-//		double Kp = 7;
+//		double Kp = 14;
 		double Ki = 0;
 		double Kd = 0;
 		
 		for(double Kp = 1; Kp < 15; Kp++) {
-//		for(double Kd = 100; Kd < 1000; Kd = Kd + 10) {
+//		for(double Kd = 3; Kd < 3; Kd = Kd + 0.1) {
 
 				for(int i = 0; i < angles.length; i++) {
-					System.out.printf("P:%.1f I:%.1f D:%.1f\n", Kp, Ki, Kd);
+					System.out.printf("P%.2f I%.2f D%.2f\n", Kp, Ki, Kd);
 
-//					result = PIDController1.PID(rand.nextInt()%360, Kp, Ki, Kd, 100);
+////					result = PIDController1.PID(rand.nextInt()%360, Kp, Ki, Kd, 100);
 					result = PIDController1.PID(angles[i], Kp, Ki, Kd, 100);
 
 					System.out.println(result);
 					Delay.msDelay(1000);
-//					Button.waitForAnyPress();	// press any button to exit
+////					Button.waitForAnyPress();	// press any button to exit
 				}
 			}
 
--- src/L2/PIDController.java
+++ src/L2/PIDController.java
@@ -18,7 +18,10 @@ public class PIDController {
 	double integral = 0;		// the integral error
 	double delta_t = 0;		// delta t
 	int power = 0;			// power to motor
-
+	int prevPower = 0;		// power from previous sample
+	int powerMin = 10;		// minimum power to get PID controller to finish
+	int sampleRate = 10;		// enforced minimum sample rate, 
+							// > 1 ms to ensure derivative never divides by 0, and avoid oversample
 	
 	/*
 	 * 
@@ -27,7 +30,6 @@ public class PIDController {
 		M = motor;
 		timer = new Stopwatch();
 	}
-	
 	
 	/*
 	 * PID controller
@@ -39,45 +41,36 @@ public class PIDController {
 		prevError = error;	// error from previous sample
 		derivative = 0;		// the derivative error
 		integral = 0;		// the integral error
-		delta_t = 1;			// delta t
+		delta_t = 0;			// delta t
 		power = 0;			// power to motor
-		
+
 		M.resetTachoCount();
 		timer.reset();
-		
+
 		while (error != 0 || prevError != 0){
-			integral = integral + (error * delta_t);
-			derivative = (error - prevError) / delta_t;
-			power = (int) (Math.min(powerMax, Math.abs((Kp * error) + (Ki * integral) + (Kd * derivative))));
-			M.setPower(isPos(error) * power);
-			Delay.msDelay(10);					// this delay! grrr, works reasonably well when < 15
-												// the dervative only seems to have a meaningful effect
-												// when it is removed altogether, but removing it altogether
-												// causes bad behaviour in otherways, and it is also unclear
-												// why the derivative is not functioning meaningfully when
-												// there is a delay, even of as little as 1 ms!
+			integral = integral + (error * delta_t/100);
+			derivative = (error - prevError) / (delta_t/100);
+			
+			power = (int) Math.abs((Kp * error) + (Ki * integral) + (Kd * derivative));
+			M.setPower(isPos(error) * (Math.min(powerMax, powerMin + power)));
 			
 			// get error and delta t
+			//System.out.println("P: " + (int)(Kp * error) + "\nI: " + (int)(Ki * integral) + "\nD: " + (int)(Kd * derivative));
+			Delay.msDelay(sampleRate);
 			prevError = error;
 			error = target-M.getTachoCount();
 			delta_t = timer.elapsed();
 			timer.reset();
 		}
 		
-		M.setPower(0); // halt motor
+		M.setPower(0); 						// halt motor
 		Delay.msDelay(500);					// wait for motor to fully halt
 		error = target-M.getTachoCount();	// get error again because the motor has possibly moved
 											// a little after we exited the while loop
 		return ("Error: " + error + "\nTacho: " + M.getTachoCount() + "\n");
 	}
 	
-	/*
-	 * is e pos or neg? Used to get the direction of the error... but come to think of it the Math.abs above might be 
-	 * the reason things aren't working for me. Try removing that, I can't recall why I did that in the first place!
-	 * Oooooh, because the clipping using Math.min currently needs an unsigned magnitude to work properly... maybe
-	 * there is a better way of dividing that up... but really, the direction of the error should be the direction
-	 * of the power so that we head closer toward the target every time unless we overshoot.
-	 */
+	// get the sign of a double
 	int isPos(double e) {
 		if (e < 0){
 			return -1;
