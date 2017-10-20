5e9d7efeecb787efb974f9cfbb0401658f414958
Laura Petrich <laura.petrich@gmail.com>
2017-10-19 20:42:11 -0600

    work it harder make it better

--- src/L2/fKine.java
+++ src/L2/fKine.java
@@ -3,9 +3,10 @@ package L2;
 import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
-import lejos.utility.Stopwatch;
+//import lejos.utility.Stopwatch;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -23,7 +24,7 @@ public class fKine {
 		double coord[] = new double[2];
 		int powerMax;
 		int offset;
-		Stopwatch timer = new Stopwatch();
+//		Stopwatch timer = new Stopwatch();
 		
 		Motor(PIDCfKine PID, int target, int offset, double Kp, double Ki, double Kd, int powerMax) {
 			this.PID = PID;
@@ -36,18 +37,18 @@ public class fKine {
 		}
 		
 		public double[] call() {
-			timer.reset();
+//			timer.reset();
 			coord = PID.PID(target, offset, Kp, Ki, Kd, powerMax, false);
 //			System.out.println("\nTimer: " + timer.elapsed());
 			return coord;
 		}
 	}	
-	public static void main(String[] args) throws Exception{
-		//////////////////////////////////
-		int[] target = {0, -45};
-//		int[] target = {90, 90};
-		//////////////////////////////////
+
+	public static void fkine(int A, int B) {
+		int[] target = {A, B};
 		int[] offset = {41, 72};
+	    double[] p1 = null;
+	    double[] p2 = null;
 		double[] homeBase = {-74, 0};
 		double[] power = {0, 0};
 		double[] e = {-0.08604, 1.238, 1.272, -0.2078};
@@ -55,14 +56,20 @@ public class fKine {
 		double[] d = {-3.806, 0.2893, -0.004203,  1.888e-05};
 		double[] endEffector = {0,0};
 		double[][] Kval = new double[3][2];
-		double x = Math.abs(target[0]/target[1]);
-		double powerRatio = (e[0]*Math.exp(e[1]*x) + e[2]*Math.exp(e[3]*x));
-		
-		power[1] = 100;
-		power[0] = power[1]*powerRatio;
-		if (powerRatio > 1) {
-			power[0] = (power[1]/power[0])*100; // scale to within range [0 100]
+		if (target[1] != 0) {
+			double x = Math.abs(target[0]/target[1]);
+			double powerRatio = (e[0]*Math.exp(e[1]*x) + e[2]*Math.exp(e[3]*x));
+			power[1] = 100;
+			power[0] = power[1]*powerRatio;
+			if (powerRatio > 1) {
+				power[0] = (power[1]/power[0])*100; // scale to within range [0 100]
+			}
 		}
+		else { 
+			power[1] = 0; 
+			power[0] = 100; 
+		}
+
 		// calculate Kp and Kd values for each joint
 		for (int i = 0; i < 2; i++) {
 			Kval[0][i] = p[0] + (p[1] * power[i]) + (p[2] * Math.pow(power[i], 2)) + (p[3] * Math.pow(power[i], 3));
@@ -80,27 +87,33 @@ public class fKine {
 		PIDCfKine PIDController2 = new PIDCfKine(M2);
 		
 		ExecutorService executor = Executors.newFixedThreadPool(3);
-	    Callable<double[]> coord1 = new Motor(PIDController1, offset[0] * target[0], offset[0], Kval[0][0], Kval[1][0], Kval[2][0], (int) power[0]);
+		// first joint
+	    Callable<double[]> coord1 = new Motor(PIDController1, offset[0] * target[0], offset[0], 2.5, 0.0, 2.0, (int) power[0]);
 	    Future<double[]> future1 = executor.submit(coord1);
-	    double[] p1 = future1.get();
+		try {
+			p1 = future1.get();
+		} catch (InterruptedException | ExecutionException e1) {
+			// catch block
+			e1.printStackTrace();
+		}
 //		System.out.printf("\n\nx: %.3f  y: %.3f", p1[0], p1[1]);
-	    
+	    // second joint
 		Callable<double[]> coord2 = new Motor(PIDController2, offset[1] * target[1], offset[1], Kval[0][1], Kval[1][1], Kval[2][1], (int) power[0]);
 	    Future<double[]> future2 = executor.submit(coord2);
-	    double[] p2 = future2.get();
+		try {
+			p2 = future2.get();
+		} catch (InterruptedException | ExecutionException e1) {
+			// catch block
+			e1.printStackTrace();
+		}
 		
 	    for (int i = 0; i < 2; i++) {
 			endEffector[i] = p1[i] + p2[i] - homeBase[i];
 	    }
 		System.out.printf("End Effector Position: \n(%.2f, %.2f)\n", endEffector[0], endEffector[1]);
-
-		
 		executor.shutdown();
-
-		Delay.msDelay(2000);
+		Delay.msDelay(2000);	
 		
-	
 	}
-
 }
 
