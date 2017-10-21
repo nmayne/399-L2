48ea77b657c6047af7dc67bacdb879eebe9b1283
Laura Petrich <laura.petrich@gmail.com>
2017-10-20 21:13:49 -0600

    forgot about all these

--- src/L2/fKine.java
+++ src/L2/fKine.java
@@ -46,35 +46,37 @@ public class fKine {
 
 	public static void fkine(int A, int B) {
 		int[] target = {A, B};
-		int[] offset = {41, 72};
+		int[] offset = {62, 69};
 	    double[] p1 = null;
 	    double[] p2 = null;
-		double[] homeBase = {-74, 0};
-		double[] power = {0, 0};
+		double[] homeBase = {-72, 0};
+		double[] power = {100, 100};
 		double[] e = {-0.08604, 1.238, 1.272, -0.2078};
-		double[] p = {3.259, 0.1189, -0.002427, 1.354e-05};
-		double[] d = {-3.806, 0.2893, -0.004203,  1.888e-05};
-		double[] endEffector = {0,0};
+        double[] Kp = {0.0004637, -0.08011, 5.18};
+        double[] Kd = {-0.0001348, 0.02578, 0.6731};	
 		double[][] Kval = new double[3][2];
-		if (target[1] != 0) {
-			double x = Math.abs(target[0]/target[1]);
-			double powerRatio = (e[0]*Math.exp(e[1]*x) + e[2]*Math.exp(e[3]*x));
-			power[1] = 100;
-			power[0] = power[1]*powerRatio;
-			if (powerRatio > 1) {
-				power[0] = (power[1]/power[0])*100; // scale to within range [0 100]
-			}
-		}
-		else { 
-			power[1] = 0; 
-			power[0] = 100; 
-		}
+//		if (target[1] != 0) {
+//			double x = Math.abs(target[0]/target[1]);
+//			double powerRatio = (e[0]*Math.exp(e[1]*x) + e[2]*Math.exp(e[3]*x));
+//			power[1] = 100;
+////			power[0] = 50;
+//			power[0] = Math.abs(power[1]*powerRatio);
+//			System.out.printf("power0: %.0f", power[0]);
+//			if (powerRatio > 1) {
+//				power[0] = (power[1]/power[0])*100; // scale to within range [0 100]
+//				System.out.printf("power0: %.0f", power[0]);
+//			}
+//		}
+//		else { 
+//			power[1] = 0; 
+//			power[0] = 100; 
+//		}
 
-		// calculate Kp and Kd values for each joint
-		for (int i = 0; i < 2; i++) {
-			Kval[0][i] = p[0] + (p[1] * power[i]) + (p[2] * Math.pow(power[i], 2)) + (p[3] * Math.pow(power[i], 3));
+		// calculate Kp and Kd values for each motor
+		for (int i = 0; i < 2; i++) {			
+			Kval[0][i] = (Kp[0] * Math.pow(Math.abs(power[i]), 2)) + (Kp[1] * Math.abs(power[i])) + Kp[2] + 5.0;
 			Kval[1][i] = 0;
-			Kval[2][i] = d[0] + (d[1] * power[i]) + (d[2] * Math.pow(power[i], 2)) + (d[3] * Math.pow(power[i], 3));
+			Kval[2][i] = (Kd[0] * Math.pow(Math.abs(power[i]), 2)) + (Kd[1] * Math.abs(power[i])) + Kd[2] + 0.1;
 		}
 		// check the values
 //		System.out.println("\nPowerRatio: " + powerRatio + "\nJ1 Power: " + power[0] + "\nJ2 Power: " + power[1]); 
@@ -88,7 +90,7 @@ public class fKine {
 		
 		ExecutorService executor = Executors.newFixedThreadPool(3);
 		// first joint
-	    Callable<double[]> coord1 = new Motor(PIDController1, offset[0] * target[0], offset[0], 2.5, 0.0, 2.0, (int) power[0]);
+	    Callable<double[]> coord1 = new Motor(PIDController1, offset[0] * target[0], offset[0], Kval[0][1], Kval[1][1], Kval[2][1], (int) power[0]);
 	    Future<double[]> future1 = executor.submit(coord1);
 		try {
 			p1 = future1.get();
@@ -107,10 +109,33 @@ public class fKine {
 			e1.printStackTrace();
 		}
 		
-	    for (int i = 0; i < 2; i++) {
-			endEffector[i] = p1[i] + p2[i] - homeBase[i];
-	    }
-		System.out.printf("End Effector Position: \n(%.2f, %.2f)\n", endEffector[0], endEffector[1]);
+		double J1x = (2.067 * p1[0]) + 0.3235;
+		double J1y = (1.717 * p1[1]) + 0.9889;
+	    	System.out.printf("J1x: %.2f J1y: %.2f", J1x, J1y);
+	    	
+	    	double EEx;
+	    	double EEy;
+	    	
+	    if (target[1] == 0) {
+	    		EEx = J1x;
+	    		EEy = J1y;
+	    	}
+	    	else if (target[0] == 0){
+	    		EEx = -72 + p2[0];
+	    		EEy = p2[1];
+	    	}
+	    	else {
+	    		EEx = J1x - p2[0] - 72;
+	    		EEy = J1y; 
+	    	}
+
+	    	System.out.printf("\nEEx : %.0f EEy : %.0f" , EEx, EEy);
+		
+		
+//	    for (int i = 0; i < 2; i++) {
+//			endEffector[i] = p1[i] + p2[i] - homeBase[i];
+//	    }
+//		System.out.printf("\nEnd Effector Position: \n(%.2f, %.2f)\n", endEffector[0], endEffector[1]);
 		executor.shutdown();
 		Delay.msDelay(2000);	
 		
